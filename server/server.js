7333b657c64680ca65ef34d94b9271b53d466f50
SC2021 <walterlaw19@gmail.com>
2021-11-12 00:13:08 -0500

    add type defs and resolvers in schema

--- server/server.js
+++ server/server.js
@@ -18,6 +18,9 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+server.applyMiddleware({ app });
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -26,13 +29,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
-
-
 
 
 
